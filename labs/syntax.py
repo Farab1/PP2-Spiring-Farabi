5ec5b8f91679e6b8fde0387b6f2666c1fd2f69d3
Farab1 <farabisatymkul@gmail.com>
2025-01-14 23:18:36 +0600

    upd

---
+++ labs/syntax.py
@@ -0,0 +1,11 @@
+print("Input first number")
+x=int(input())
+print ("Input second number")
+y=int(input())
+
+if x>y:
+    print("first number is bigger than second")
+elif x==y:
+    print("Numbers are equal")
+else :
+    print ("second number is bigger than first")
