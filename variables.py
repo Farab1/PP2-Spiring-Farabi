c16ac82152d976c71f38005d080827d6ddfb882e
Farab1 <farabisatymkul@gmail.com>
2025-01-17 14:25:08 +0600

    syll

--- variables.py
+++ variables.py
@@ -1,10 +1,9 @@
 w = 15
 ww= "week"
-b=" ball"
-githubsubmission =11
-defense = 49
+githubsubmission = "11 ball for submission"
+defense = "49 ball for defense"
 s="syllabus have "
 p="PP2 Spring's "
 f=40
 
-print (p,s,w,ww,"and",githubsubmission,b,defense,b)
+print (p,s,w,ww,"and",githubsubmission,defense,b)
