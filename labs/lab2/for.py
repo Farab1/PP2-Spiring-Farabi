96039d7de9406fd5bfde42edf8ba12cdac064f12
Farab1 <farabisatymkul@gmail.com>
2025-01-23 21:47:55 +0500

    l9

---
+++ labs/lab2/for.py
@@ -0,0 +1,16 @@
+mylist = ['a','b','c']
+mytuple = ('1','2','3')
+myset = {'x','y','z'}
+myd = {'fruit' : 'orange' , 'color' : 'orange'}
+
+for x in mylist:
+    print(x)
+    
+for x in range(mytuple.__len__):
+    print(x)
+
+for x in myset:
+    print(x)
+    
+for x in myd:
+    print(x)
---
+++ labs/lab2/while.py
@@ -0,0 +1,6 @@
+i = int(input())
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
